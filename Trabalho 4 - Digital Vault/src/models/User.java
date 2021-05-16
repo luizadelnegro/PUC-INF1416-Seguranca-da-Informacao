fef4522fa171d4c141e22aff739a7215d6a2ea61
luizadelnegro <52245635+luizadelnegro@users.noreply.github.com>
2021-05-16 16:37:41 -0300

    PAssword possib

--- Trabalho 4 - Digital Vault/src/models/User.java
+++ Trabalho 4 - Digital Vault/src/models/User.java
@@ -3,8 +3,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.*;
 
 import controllers.MySqlController;
+
+import models.PhoneticKeyBoard;
 
 public class User {
     
@@ -12,26 +15,115 @@ public class User {
     
     private boolean validUser = false;
 
+    private boolean isBlocked;
+
+    public  ArrayList<ArrayList<String>> passwordPossibilities = new ArrayList(); //TODO 
+
+
     private static final Logger LOGGER = Logger.getLogger(User.class.getName());
 
     public User(String userEmail){
         this.userEmail = userEmail;
+        this.isBlocked = false; //TODO REMOVER
         isEmailValid();
     }
 
-    private void isEmailValid() {
+    private boolean isEmailValid() {//TODO NAO PRECISA DO RETORNO?
         MySqlController mysqlsobj = MySqlController.getInstance();
         try {
             ResultSet results = mysqlsobj.run_select_statement("SELECT * FROM user WHERE email = '" + userEmail + "'");
             
             this.validUser = results.next();
+            return true;
         } catch (SQLException e) {
             LOGGER.log(Level.SEVERE, e.getStackTrace().toString() + " " + e.getSQLState() + " " + e.getMessage());
             this.validUser = false;
+            return false;
         }
+    }
+
+    public boolean isBlocked(){
+        return this.isBlocked;
+    }
+
+    public String getEmail(){
+        return this.userEmail;
     }
 
     public boolean isValid() {
         return this.validUser;
+    }
+
+
+    public  ArrayList<ArrayList<String>> getPossibilities ( ArrayList<ArrayList<String>> selectedPassword){//TODO TORNAR PRIVATE
+        ArrayList<ArrayList<String>> passwordPossibilities = new ArrayList();
+
+         String auxRow ;
+        int j;
+        for( j = 0;j<(selectedPassword.size());j++){//cada linha da selected password      selectedPassword.size()
+            ArrayList<String> currentRowOfSelectedPassword = new ArrayList<String>(selectedPassword.get(j));
+
+            System.out.println(" currentRowOfSelectedPassword: "+currentRowOfSelectedPassword);//OK
+            //inicializa
+            if(j==0){
+                ArrayList<String> firstRow = selectedPassword.get(0);
+                    for(int i=0;i<3;i++){
+                        ArrayList<String> newRow = new ArrayList<String>();
+                        String element = firstRow.get(i);
+                        newRow.add(element);
+                        passwordPossibilities.add(newRow);
+                    }
+                    System.out.println(" PP Primeira linha: " + passwordPossibilities);//OK
+            }//se ja inicializou
+            else{
+                ArrayList<ArrayList<String>> temp = new ArrayList();
+                for (int l=0;l<3;l++){//  cada elemento da linha de selected password
+                //para cada elemento da minha lista de escolhidos eu vou combinar com meu elemento corrente e criar uma nova lista de possibilidades
+                    //pega o elemento da linha
+                    String elementOfRowOfSelectedPasswod=currentRowOfSelectedPassword.get(l);
+                    System.out.println(" elementOfRowOfSelectedPasswod El que queremos fazer a combinacao: " + elementOfRowOfSelectedPasswod);//OK
+                    //faz a combinacao com a possibilities passowrd, cada elemento d alista de possiblidades
+                    ArrayList<ArrayList<String>> auxPasswordPossibilities = new ArrayList();
+                    for(int k=0;k<passwordPossibilities.size();k++){
+                        ArrayList<String> newCombinationPossibilities = new ArrayList(passwordPossibilities.get(k));
+                        newCombinationPossibilities.add(elementOfRowOfSelectedPasswod);
+                        System.out.println("Combinacao que tem q ser add"+newCombinationPossibilities);
+                        auxPasswordPossibilities.add(newCombinationPossibilities);//acho q tem q ser do lado de fora!!!!
+                        System.out.println("Auxiliar com as possibilidades"+auxPasswordPossibilities);
+                    }       
+                    ArrayList<ArrayList<String>> aux = new ArrayList(auxPasswordPossibilities);
+                    for(int a=0;a<aux.size();a++){
+                        temp.add(aux.get(a));
+                    }                   
+                }
+                passwordPossibilities=temp;
+            }
+        }
+        
+        System.out.println("size: "+passwordPossibilities.size());
+        for(int i=0;i<passwordPossibilities.size();i++){
+            System.out.println(passwordPossibilities.get(i));
+        }
+        return passwordPossibilities;
+    }
+
+    private boolean isPasswordValid(  ArrayList<ArrayList<String>> password){//TODO mudar para private
+        //senha menor que 4 elementos e maior que 6 é sempre inválida
+        int length=password.size();
+        if (length<4 || length>6){
+            return false;
+        }
+
+        passwordPossibilities=getPossibilities(password);
+        //create array of possibilities
+        // check array of possibilities
+
+        return true;
+    }
+
+
+
+    public boolean getIsPasswordValid( ArrayList<ArrayList<String>> password){
+        return this.isPasswordValid(password);
     }
 }
--- Trabalho 4 - Digital Vault/src/ui/Console.java
+++ Trabalho 4 - Digital Vault/src/ui/Console.java
@@ -1,11 +1,13 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import models.PhoneticKeyBoard;
 import models.User;
+
 
 public class Console {
     static final String HEADER = "" +
@@ -20,32 +22,71 @@ public class Console {
 
     private static final Logger LOGGER = Logger.getLogger(Console.class.getName());
 
+
     public static void main(String args[]) {
         int selectedOption = 0;
         
         System.out.println(HEADER);
         Scanner sc= new Scanner(System.in); 
-        
+        System.out.println("(1001) Sistema Iniciado.");
+        System.out.println("(2001) Identificacao etapa 1 iniciada.");
         while (user == null) {
             System.out.println("Insira seu email:");
             String userEmail = sc.nextLine();
             user = new User(userEmail);
-            if (! user.isValid()) {
-                LOGGER.log(Level.SEVERE, "Wrong email! " + userEmail);
+            if (!user.isValid()){
+                System.out.println("(2005) Login name "+userEmail+ " não identificado.");
+                user = null;
+            } else if (user.isValid() && user.isBlocked()){
+                System.out.println("(2004) Login name "+ userEmail + " identificado com acesso bloqueado.");
                 user = null;
             }
-        }
-        PhoneticKeyBoard keyBoard = new PhoneticKeyBoard();
-        while(selectedOption != 7) {
-            System.out.println(keyBoard.getAsSingleString() + "7- END");
-            selectedOption = sc.nextInt();
-            if (selectedOption > 0 && selectedOption < 7) {
-                keyBoard.pressGroup(selectedOption);
-                keyBoard.randomizeKeys();
+            else{
+                System.out.println("(2003) Login name "+ userEmail + " lidentificadocom acesso liberado. ");
             }
 
+            System.out.println("(2002) Autenticação etapa 1 encerrada. ");
+           
+           
+
+
+            System.out.println("(3001) Autenticação etapa 2 iniciada para "+  user.getEmail() + ".");
+            PhoneticKeyBoard keyBoard = new PhoneticKeyBoard();
+            while(selectedOption != 7) {
+                System.out.println(keyBoard.getAsSingleString() + "7- END");
+                selectedOption = sc.nextInt();
+                if (selectedOption > 0 && selectedOption < 7) {
+                    keyBoard.pressGroup(selectedOption);
+                    keyBoard.randomizeKeys();
+                }
+            }
+//lucasdamo1@gmail.com
+            System.out.println(" Senha selecionada : "+keyBoard.getSelectedPassword());//TODO APAGAR AQUI E NO MODEL USER -- APENaS PARA EBUG
+            ArrayList<ArrayList<String>> password = keyBoard.getSelectedPassword();
+           // System.out.println(user.getPossibilities(password));
+            boolean isPasswordValid = user.getIsPasswordValid(password);
+            if (!isPasswordValid){//TODO TEM QUE VER sE TA BLOQUEADO
+                System.out.print("(3004) Primeiro erro da senha pessoal contabilizadopara "  + user.getEmail() + ".");
+                //TODO Contabilizar eros
+                
+            } else if (isPasswordValid && user.isBlocked()) {//TODO VER SE TA BLOQUEADO COM o pRIMEIROluca
+                System.out.println("(3007) Acesso do usuario " + user.getEmail() + " bloqueadopela autenticação etapa 2." );
+                user = null;
+            }
+            else{
+                System.out.println("(3003) Senha pessoal verificada positivamente para " + user.getEmail() + ".");
+            } 
+    
+            System.out.println("(3002) Autenticação etapa 2 encerrada para "+  user.getEmail() + ".");
+    
+
+
+
+
+
         }
 
+       
         
     }
 }
